7e751328301cd11b3888c601bf01a223f13131c3
occamsrazor1776 <occamsrazor1776@gmail.com>
2017-06-16 11:18:30 -0400

    User Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   get 'pages/home'
 
+
   root 'pages#home'
 
 
-  devise_for :users
+  devise_for :users, :path => '',
+             :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'}
+             
+             
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  
+ resources :users, only: [:show]
 
 end
